30396d3f8f6ee6d23a1611db1005e796aa4468c7
keizi <keizi81@gmail.com>
2020-09-04 00:15:54 +0900

    add Detail Pages

---
+++ src/app/pokemon-detail/pokemon-detail.component.ts
@@ -0,0 +1,23 @@
+
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { POKEMONS } from 'src/models/pokemons';
+
+@Component({
+  selector: 'app-pokemon-detail',
+  templateUrl: './pokemon-detail.component.html',
+  styleUrls: ['./pokemon-detail.component.scss'],
+})
+export class PokemonDetailComponent implements OnInit {
+  pokemons = POKEMONS;
+  pokemon;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((map) => {
+      const id = +map.get('id');
+      this.pokemon = this.pokemons[id - 1];
+    });
+  }
+}
